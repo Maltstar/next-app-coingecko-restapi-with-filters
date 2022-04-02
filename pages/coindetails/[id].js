4b2d84460a0e9376cbbaa506a25fd4019edd8950
LohremYpsum <102668432+LohremYpsum@users.noreply.github.com>
2022-04-02 19:09:58 +0200

    added conditionals to Coindetails renderoutput

--- pages/coindetails/[id].js
+++ pages/coindetails/[id].js
@@ -18,15 +18,23 @@ export async function getServerSideProps(context) {
 const Coindetails = ({ coin }) => {
   const description = coin.description.en;
   const sanitizer = dompurify.sanitize;
+  const isGenesisDate = coin.genesis_date;
+  const developerData = coin.developer_data;
+  const isHomepage = coin.links.homepage;
+  //console.log(developerData);
 
   // tried purifying hrefs. doesnt work: https://linguinecode.com/post/complete-guide-react-dangerouslysetinnerhtml
   //dangerouslySetInnerHTML={{ __html: sanitizer(description) }}
   return (
-    <Layout title={`${coin.id} | Genesis-Date: ${coin.genesis_date}`}>
+    <Layout
+      title={`${coin.id} | Genesis-Date: ${
+        isGenesisDate ? isGenesisDate : 'N/A'
+      }`}
+    >
       <div className="flex-container">
         <p>{description}</p>
         <ul>
-          <li>Offizielle Website: {coin.links.homepage}</li>
+          <li>Offizielle Website: {isHomepage ? isHomepage : 'Website N/A'}</li>
         </ul>
       </div>
       <div className="flex-container">
@@ -34,6 +42,7 @@ const Coindetails = ({ coin }) => {
           chartTitle="General Score in %"
           singleCryptoStats={coin}
         />
+
         <ChartDeveloperData
           chartTitle="Entwickler Statistik (Abs. Zahlen)"
           singleCryptoStats={coin}
